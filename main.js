2a01b2ff1b465f5cee7fea421df1727cc11a55ec
Anhlh <anhlh011190@gmail.com>
2017-11-14 09:12:10 +0700

    Anhlh2: Call giữa 2 client

--- main.js
+++ main.js
@@ -9,13 +9,37 @@ function playStream(idVideoTag ,stream){
     video.play();
 }
 
-openStream()
-.then(stream => {
-    playStream("localStream", stream);
-})
+// openStream()
+// .then(stream => {
+//     playStream("localStream", stream);
+// })
 
 const peer = new Peer({key: "peerjs", host: 'peeranhlh2.herokuapp.com', secure: true, port: 443});
 
 peer.on('open', id => {
     $('#myPeer').append(id);
-})
+});
+
+//caller
+$('#call').click(()=>{
+    let id = $('#remoteId').val();
+    openStream()
+    .then(stream => {
+        playStream("localStream", stream);
+        let call = peer.call(id, stream);
+        call.on('stream', remoteStream => {
+            playStream("remoteStream", remoteStream);
+        });
+    })
+});
+
+peer.on('call', call => {
+    openStream()
+    .then(stream => {
+        call.answer(stream);
+        playStream("localStream", stream);
+        call.on('stream', remoteStream => {
+            playStream("remoteStream", remoteStream);
+        });
+    })
+});
